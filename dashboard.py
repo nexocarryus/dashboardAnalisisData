4ae3946445e1abe7a91362e735cc0813752c4f28
Naufal Dzakwan Zakianto <94432605+nexocarryus@users.noreply.github.com>
2025-03-14 21:36:03 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import seaborn as sns
 import streamlit as st
-
+sns.set(font_scale=6)
 
 # Helper function yang dibutuhkan untuk menyiapkan berbagai dataframe
 def create_bySeason_df(df):
@@ -97,21 +97,45 @@ def create_byHourandWeek(df):
     pivot_table.columns = pivot_table.columns.map(weekday_names)
     return pivot_table
 
+# Load cleaned data
+df_day = pd.read_csv("df_dayClean.csv")
+df_day['dteday'] = pd.to_datetime(df_day['dteday'])
+df_day.sort_values(by="dteday", inplace=True)
+df_day.reset_index(inplace=True)
+
+
+df_hour = pd.read_csv("df_hourClean.csv")
+df_hour['dteday'] = pd.to_datetime(df_hour['dteday'])
+df_hour.sort_values(by="dteday", inplace=True)
+df_hour.reset_index(inplace=True)
+
+combine_df = pd.concat([df_day['dteday'], df_hour['dteday']])
+
+min_date = combine_df.min()
+max_date = combine_df.max()
+
 with st.sidebar:
     #menambahkan logo perusahaan
     st.image("RentBikeLogo.png")
 
-# Load cleaned data
-df_day = pd.read_csv("df_dayClean.csv")
-df_day['dteday'] = pd.to_datetime(df_day['dteday'])
-df_hour = pd.read_csv("df_hourClean.csv")
+    #mengambil start_date & end_date dari date_input
+    start_date, end_date= st.date_input(
+        label="Rentang Waktu", min_value=min_date, max_value=max_date,
+        value=[min_date, max_date]
+    )
 
-# # Menyiapkan berbagai dataframe
-bySeason_df = create_bySeason_df(df_day)
-byWeather_df = create_byWeather(df_day)
-byDay_df = create_byDay(df_day)
-userbyHour = createUserPattern_byHour(df_hour)
-byHourandWeek_df = create_byHourandWeek(df_hour)
+main_df_day = df_day[(df_day['dteday'] >= str(start_date)) & 
+                      (df_day['dteday'] <= str(end_date))]
+main_df_hour = df_hour[(df_hour['dteday'] >= str(start_date)) & 
+                      (df_hour['dteday'] <= str(end_date))]
+
+
+# Menyiapkan berbagai dataframe
+bySeason_df = create_bySeason_df(main_df_day)
+byWeather_df = create_byWeather(main_df_day)
+byDay_df = create_byDay(main_df_day)
+userbyHour = createUserPattern_byHour(main_df_hour)
+byHourandWeek_df = create_byHourandWeek(main_df_hour)
 
 
 #menampilkan header dashboard
@@ -147,8 +171,8 @@ with colPeakDay:
     colors = ['lightgrey' if cnt != byDay_df['cnt'].max() else 'lightgreen' for cnt in byDay_df['cnt']]
     plt.figure(figsize=(34, 30))
     sns.barplot(x='weekday', y='cnt', data=byDay_df, palette=colors)  
-    plt.xlabel('')
-    plt.ylabel('')
+    plt.xlabel('Hari')
+    plt.ylabel('Total penyewa sepeda')
     plt.title('berdasarkan hari')
     plt.xticks(rotation=20, ha='right')
     st.pyplot(plt)
@@ -160,17 +184,17 @@ plt.figure(figsize=(50, 20))
 plt.plot(userbyHour['hr'], userbyHour['casual'], label='Casual', color='blue', linewidth=2.5)
 plt.plot(userbyHour['hr'], userbyHour['registered'], label='Registered', color = 'green', linewidth=2.5)
 plt.xlabel('Jam dalam sehari', size = 50)
-plt.ylabel('Rata rata penyewaan sepeda', size = 50)
+plt.ylabel('Rata rata penyewa sepeda', size = 50)
 plt.title('Registered user vs casual user berdasarkan jam', size = 60)
 plt.legend()
 plt.grid(True)
 st.pyplot(plt)
 
 plt.figure(figsize=(50, 40))
-sns.scatterplot(x='temp', y='casual', data=df_day, color='blue', label='Casual', alpha=0.7, s=200)
-sns.scatterplot(x='temp', y='registered', data=df_day, color='green', label='Registered', alpha=0.7, s=200)
+sns.scatterplot(x='temp', y='casual', data=main_df_day, color='blue', label='Casual', alpha=0.7, s=200)
+sns.scatterplot(x='temp', y='registered', data=main_df_day, color='green', label='Registered', alpha=0.7, s=200)
 plt.xlabel('Temperatur (celcius)')
-plt.ylabel('Penyewaan sepeda')
+plt.ylabel('Total Penyewa sepeda')
 plt.title('Registered user vs casual user berdasarkan temperatur')
 plt.legend()
 plt.grid(True)
@@ -184,8 +208,8 @@ weather_names = {
 }
 
 plt.figure(figsize=(50, 20))
-sns.boxplot(x='weathersit', y='casual', data=df_day, color='blue', label='Casual')
-sns.boxplot(x='weathersit', y='registered', data=df_day, color='lightgreen', label='Registered')
+sns.boxplot(x='weathersit', y='casual', data=main_df_day, color='blue', label='Casual')
+sns.boxplot(x='weathersit', y='registered', data=main_df_day, color='lightgreen', label='Registered')
 
 xticklabels = plt.gca().get_xticklabels()
 
@@ -193,49 +217,49 @@ for label in xticklabels:
     label.set_text(weather_names.get(int(label.get_text()), label.get_text()))
 
 plt.gca().set_xticklabels(xticklabels)
-plt.xlabel('')
-plt.ylabel('')
-plt.title('Penyewaan sepeda registered user vs casual user berdasarkan cuaca')
+plt.xlabel('Kondisi cuaca')
+plt.ylabel('Total Penyewa sepeda')
+plt.title('Registered user vs casual user berdasarkan kondisi cuaca')
 plt.legend()
 st.pyplot(plt)
 
-#trend penyewaan sepeda dari tahun 2011 sampai tahun 2012
-st.subheader('Bike rental trend from 2011-2012')
+#trend penyewaan sepeda 
+st.subheader('Bike rental trend')
+
+
 plt.figure(figsize=(40, 20))
-plt.plot(df_day['dteday'], df_day['cnt'], color='green')
-plt.xlabel('Date', size=15)
-plt.ylabel('Count', size=15)
+plt.title('Tren penyewaan sepeda secara keseluruhan')
+plt.plot(main_df_day['dteday'], main_df_day['cnt'], color='green')
+plt.xlabel('Rentang waktu')
+plt.ylabel('Total penyewa sepeda')
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m')) 
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))  
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout() 
 st.pyplot(plt)
 
-df_day['month'] = df_day['dteday'].dt.month
-monthly_trends = df_day.groupby('month')[['casual', 'registered', 'cnt']].sum().reset_index()
+main_df_day['month'] = main_df_day['dteday'].dt.month
+monthly_trends = main_df_day.groupby('month')[['casual', 'registered', 'cnt']].sum().reset_index()
 
 plt.figure(figsize=(50, 20))
 plt.plot(monthly_trends['month'], monthly_trends['casual'], label='Casual', marker='o', color='blue',  linewidth=2.5)
 plt.plot(monthly_trends['month'], monthly_trends['registered'], label='Registered', marker='o', color = 'green',  linewidth=2.5)
 plt.plot(monthly_trends['month'], monthly_trends['cnt'], label='Total', marker='o', color='orange',  linewidth=2.5)
-plt.xlabel('bulan')
-plt.ylabel('')
-plt.title('tren penyewaan sepeda berdasarkan bulan')
+plt.xlabel('Bulan')
+plt.ylabel('Total penyewa sepeda')
+plt.title('Tren penyewaan sepeda berdasarkan bulan & jenis pengguna')
 plt.xticks(monthly_trends['month'])
 plt.legend()
 plt.grid(True)
 st.pyplot(plt)
 
 #pola volume penyewaan sepeda berdasarkan jam dan hari
-st.subheader('Bike rental volume pattern')
+st.subheader('Bike rental frequency pattern')
 plt.figure(figsize=(50, 40))
-sns.heatmap(byHourandWeek_df, annot=True, fmt="d", cmap="YlGnBu")
-plt.title('Volume rental sepeda berdasarkan jam dan hari')
+sns.heatmap(byHourandWeek_df, annot=True, fmt=".0f", cmap="YlGnBu")
+plt.title('Frekuensi penyewaan sepeda berdasarkan jam dan hari')
 plt.ylabel('Jam')
-plt.xlabel('')
+plt.xlabel('Hari')
 st.pyplot(plt)
-
-
-
 
 
